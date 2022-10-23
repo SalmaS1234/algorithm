e796fdae4b5e0f4b32dea97724407ea56537ae66
SalmaS1234 <100144021+SalmaS1234@users.noreply.github.com>
2022-10-23 22:04:06 +0600

    Add files via upload

---
+++ Read-File.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include<time.h>
+using namespace std;
+
+int main(){
+
+srand(time(0));
+
+freopen("100-int.txt","r",stdin);
+int a[100];
+
+for(int i=0;i<100;i++){
+
+   cin>>a[i];
+
+}
+
+for(int i=0;i<100;i++){
+
+    cout<<a[i]<<endl;
+}
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
