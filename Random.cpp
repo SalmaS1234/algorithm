d6454e7664c953bf2a4e6311090bc02c8723ab33
SalmaS1234 <100144021+SalmaS1234@users.noreply.github.com>
2022-10-23 20:32:07 +0600

    Add files via upload

---
+++ Random.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#include<time.h>
+using namespace std;
+
+int main(){
+
+srand(time(0));
+
+freopen("100-int.txt","w",stdout);
+
+for(int i=0;i<100;i++){
+
+cout<<rand()%100+1<<endl;
+
+}
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
